b4bb7760ba139af0f751c16c7f6bd2490a9dcca2
wandss <wandss@gmail.com>
2019-03-19 01:31:59 -0300

    Adds modal to Home view

--- frontend/cncweb/src/components/Modal.vue
+++ frontend/cncweb/src/components/Modal.vue
@@ -1,54 +1,57 @@
 <template>
-    <Overlay :show="show">
-        <div :class="['modal-dialog', modalSize]">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">
-                        {{title}}
-                    </h5>
-                    <button type="button" class="close" @click="$emit('click')">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <slot name="body"></slot>
-                </div>
-                <div class="modal-footer" >
-                    <slot name="footer"></slot>
-                </div>
-            </div>
+  <Overlay :show="show">
+    <div :class="['modal-dialog', modalSize]">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">
+            {{title}}
+          </h5>
+          <button type="button" class="close" @click="$emit('click')">
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div>
-    </Overlay>
+        <div class="modal-body">
+          <slot name="body"></slot>
+        </div>
+        <div class="modal-footer" >
+          <slot name="footer"></slot>
+        </div>
+      </div>
+    </div>
+  </Overlay>
 </template>
 <script>
 import Overlay from '@/components/Overlay';
 export default{
-    name:'Modal',
-    components:{Overlay},
-    props:{
-        title:{
-            type:String,
-            required:true,
-            default:'Modal Title'
-        },
-        modalSize:{
-            defalt:'modal-sm'
-        },
-        hasFooter:{
-            type:Boolean,
-            default:false,
-        },
-        show:{
-            type:Boolean,
-            default:false,
-            required:true,
-        }
+  name:'Modal',
+  components:{Overlay},
+  props:{
+    title:{
+      type:String,
+      required:true,
+      default:'Modal Title'
+    },
+    modalSize:{
+      defalt:'modal-sm'
+    },
+    hasFooter:{
+      type:Boolean,
+      default:false,
+    },
+    show:{
+      type:Boolean,
+      default:false,
+      required:true,
     }
+  }
 
 }
 </script>
 <style>
 .modal-full{
-    max-width:98% !important;
+  max-width:98% !important;
+}
+button:focus{
+  outline:0;
 }
 </style>
--- frontend/cncweb/src/views/Home.vue
+++ frontend/cncweb/src/views/Home.vue
@@ -1,31 +1,51 @@
 <template>
   <div class="container home">
-    <form action="">
-      <div class="form-group">
-        <upload label="Upload GCode File" name="upload"
-         @change="handleUpload"
-        />
-        <base-btn type="submit">
-          Start Plotting
-        </base-btn>
-      </div>
-    </form>
+    <div class="form-group">
+      <h3>Hello!!!</h3>
+      <p>This app controls a Mini CNC Ploting  machine, made with two DVD Drives.</p>
+      <p>To start ploting just click the button bellow</p>
+       <modal :show="showModal" @click="showModal=false"  modalSize="modal-lg"
+       title="Start your engines...">
+         <div slot="body">
+          <upload label="Upload GCode File" name="upload"
+           @change="handleUpload"
+           />
+         </div>
+         <div slot="footer" >
+          <base-btn cssClass="btn btn-warning" :disabled="disabledStartButton"
+            @click="startPlotting">
+            Start Plotting
+          </base-btn>
+         </div>
+       </modal>
+      <base-btn @click="showModal=!showModal">
+        Start Plotting
+      </base-btn>
+    </div>
   </div>
 </template>
 <script>
 import BaseBtn from '@/components/baseButton.vue'
+import Modal from '@/components/Modal.vue'
 import Upload from '@/components/Upload.vue'
 export default {
   name: 'home',
-  components: {BaseBtn, Upload},
+  components: {BaseBtn, Modal, Upload},
   data(){
     return {
       uploadedFile: '',
+      showModal: false,
+      disabledStartButton: true,
     }
   },
   methods: {
     handleUpload(ark){
       console.log(ark)
+      this.disabledStartButton = false
+    },
+    startPlotting(){
+      console.log('Stat Plotting.......')
+      console.log('Open Loader')
     }
   }
 }
